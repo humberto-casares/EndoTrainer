4f1a78b509fdcb8f00c8bad804c76dee360f602a
Humberto Casares <78005862+humberto-casares@users.noreply.github.com>
2023-06-09 13:28:00 -0700

    Update results.php

--- results.php
+++ results.php
@@ -81,8 +81,12 @@
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Dashboard</h1>
-                        
+                        <h1 class="mt-4">Results</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="main.php">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Results</li>
+                        </ol>
+
                         <div class="row">
                             <?php
                                 $mysqli = conectar();
